d46ef4f45965890f01c46760cfa4f58bdafe03c0
Jayani <jayanipatel23@gmail.com>
2021-05-06 00:19:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('D:\ModelDeployment\index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
 
 # In[ ]:
